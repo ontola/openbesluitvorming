df9633a1065859562b51101a9338b14a9e38ac58
Joep Meindertsma <joepmeindertsma@gmail.com>
2019-03-20 19:17:55 +0100

    Resize PDF!

--- front/src/Components/PDFViewer.tsx
+++ front/src/Components/PDFViewer.tsx
@@ -1,4 +1,6 @@
-import * as React from "react";
+import React, { PureComponent } from "react";
+import throttle from "lodash.throttle";
+import Resizable from "re-resizable";
 const { Document, Page, pdfjs } = require("react-pdf");
 // tslint:disable-next-line:max-line-length
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -10,40 +12,77 @@ export interface PDFViewerProps {
 export interface PDFViewerState {
   numPages: null | number;
   pageNumber: number;
+  width: number;
 }
 
 interface OnLoadSuccessType {
   numPages: number;
 }
 
-export default class PDFViewer extends React.Component<PDFViewerProps, PDFViewerState> {
+class PDFViewer extends PureComponent<PDFViewerProps, PDFViewerState> {
+  private pdfWrapper: React.RefObject<HTMLInputElement>;
   constructor(props: PDFViewerProps) {
     super(props);
-
     this.state = {
-      numPages: null,
+      width: 0,
       pageNumber: 1,
+      numPages: null,
     };
+    this.pdfWrapper = React.createRef();
+  }
+
+  componentDidMount () {
+    this.setDivSize();
+    window.addEventListener("resize", throttle(this.setDivSize, 500));
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener("resize", throttle(this.setDivSize, 500));
+  }
+
+  setDivSize = () => {
+    this.pdfWrapper.current && this.setState({
+      width: this.pdfWrapper.current.getBoundingClientRect().width,
+    });
   }
 
   onDocumentLoadSuccess = (args: OnLoadSuccessType) => {
     this.setState({ numPages: args.numPages });
   }
 
-  public render() {
-    const { pageNumber, numPages } = this.state;
-
+  render() {
     return (
-      <div>
-        <p>Page {pageNumber} of {numPages}</p>
-        <Document
-          file={this.props.url}
-          onLoadSuccess={this.onDocumentLoadSuccess}
-          onLoadError={(error: any) => alert('Error while loading document! ' + error.message)}
+      <Resizable
+        size={{ width: this.state.width || 100, height: "100%" }}
+        onResizeStop={(e, direction, ref, d) => {
+          this.setState({
+            width: this.state.width + d.width || 100 + d.width,
+          });
+        }}
+        enable={{ left: true}}
+      >
+        <div
+          onKeyDown={(e: any) => console.log(e.keyCode)}
+          id="row"
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            overflow: "scroll",
+          }}
         >
-          <Page pageNumber={pageNumber} />
-        </Document>
-      </div>
+          <div id="pdfWrapper" style={{ width: "100%" }} ref={this.pdfWrapper}>
+            <Document
+              file={this.props.url}
+              onLoadSuccess={this.onDocumentLoadSuccess}
+            >
+              <Page pageIndex={1} width={this.state.width} />
+            </Document>
+          </div>
+        </div>
+      </Resizable>
     );
   }
 }
+
+export default PDFViewer;
