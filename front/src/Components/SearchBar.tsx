20ffde64285f8cea477c3e162771dc105ce3f736
Joep Meindertsma <joepmeindertsma@gmail.com>
2019-05-14 16:14:56 +0200

    #17 Use simple_query_string

--- front/src/Components/SearchBar.tsx
+++ front/src/Components/SearchBar.tsx
@@ -1,18 +1,34 @@
 import * as React from "react";
 import { DataSearch } from "@appbaseio/reactivesearch";
 
+export const queryGenerator = (searchTerm: string) => {
+  const query = {
+    query: {
+      simple_query_string: {
+        fields,
+        default_operator: "or",
+        query: searchTerm,
+      },
+    },
+  };
+  return query;
+};
+
+const fields = ["text", "title", "description", "name"];
+
 const SearchBar: React.FunctionComponent = () => {
   return (
     <DataSearch
       className="SearchBar"
       componentId="searchbox"
-      debounce={400}
+      debounce={1200}
       showFilter={false}
-      dataField={["text", "title", "description", "name"]}
+      dataField={fields}
       highlight
       autosuggest={false}
       placeholder="Zoek in 109 gemeenten.."
       URLParams={true}
+      customQuery={queryGenerator}
       customHighlight={() => ({
         highlight: {
           pre_tags: ["<mark>"],
