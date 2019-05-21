616e1e712d4e834ca9b81c969414cb773a236777
Joep Meindertsma <joepmeindertsma@gmail.com>
2019-05-21 08:24:07 +0200

    MediaObject more props

--- front/src/Components/Views/MediaObject.tsx
+++ front/src/Components/Views/MediaObject.tsx
@@ -3,6 +3,7 @@ import React, { PureComponent } from "react";
 
 import { NS } from "../../LRS";
 import Resource from "../Topologies/ResourceTopology";
+import Labels from "../Topologies/LabelsTopology";
 
 // Mostly PDF objects
 class MediaObject extends PureComponent {
@@ -14,7 +15,12 @@ class MediaObject extends PureComponent {
         <Resource>
           <Property label={[NS.schema("name"), NS.schema("label")]} /><br />
           PDF document <br/>
-          <Property label={NS.schema("dateModified")} /><br />
+          <Labels>
+            <Property label={NS.schema("dateModified")} />
+            <Property label={NS.schema("fileSize")} />
+            <Property label={NS.schema("encodingFormat")} />
+            <Property label={NS.schema("contentUrl")} />
+          </Labels>
         </Resource>
         <Property label={NS.schema("isBasedOn")} />
       </React.Fragment>
