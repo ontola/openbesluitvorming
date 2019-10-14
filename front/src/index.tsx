bc62987fc719903c915eb9b6a2ebb939bbc61bce
Joep Meindertsma <joep@ontola.io>
2019-10-14 14:32:42 +0200

    Add globalThis polyfill #40

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -7,6 +7,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+require('globalthis/polyfill')();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
