d6c1e8b3e94cedce7126864556de214a7fecdaf7
Hendrik Grondijs <hendrik@stamkracht.com>
2019-10-25 14:29:55 +0200

    layout things

--- front/src/Components/Glossarium.tsx
+++ front/src/Components/Glossarium.tsx
@@ -52,23 +52,46 @@ class Glossarium extends React.PureComponent<{}, MState> {
 
   render() {
     return (
-      <div>
-        <div className="Glossarium">
-          <h1>Huidige selectie</h1>
-          <input 
-            className="selectedText"
-            value={this.state.selectedText}
-            onChange={this.onChange}></input>
-          <Button
-            className="evaluateSelection"
-            onClick={this.evaluateSelection}>
-              Evaluate
-          </Button>
+      <div className="Glossarium">
+        <div className="glossarium-container">
+          <div className="selection-container">
+            <h1>Huidige selectie</h1>
+            <input 
+              className="selected-text"
+              value={this.state.selectedText}
+              onChange={this.onChange}/>   
+            <Button
+              className="evaluate-selection-button"
+              onClick={this.evaluateSelection}>
+                Evaluate
+            </Button>
+          </div>
 
-          <h1>Informatie</h1>
+          <div className="definition-container">
+            <div className="definition-title">
+              <b>[canonical_name] ([abbreviation])</b>
+            </div>
+            {/* <div className="definition-source">
+              <i>Link naar bron</i>
+              <p>... de toetsende partijen zoals <b>Inspectie Leefomgeving en Transport (ILT)</b> en de Regionale Uitvoeringsdiesnt (RUD) moet verder gepland ...</p>
+            </div> */}
+            <div className="definition">
+              <i>Link naar bron</i>
+              <p>[description]</p>
+            </div>
+          </div>
 
-          <div className="container">
-            {this.state.information}
+          <div className="linked-data-container">
+            <div className="wiki-summary">
+              <p>De Inspectie Leefomgeving en Transport (ILT), een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastruc een agentschap van het Ministerie van Infrastructuur en Waterstaat, bewaakt en stimuleert de naleving van wet- en regelgeving voor een veilige en duurzame leefomgeving en transport. Zij is op 1 januari 2012 ontstaan door samenvoeging van de Inspectie Verkeer en Waterstaat (IVW) en de VROM-inspectie nadat beider ministeries waaronder ze vielen werden samengevoegd onder het Kabinet-Rutte I. Het is daarmee een van de Nederlandse rijksinspecties. In 2013 werd het Volkshuisvestelijk Toezicht aan de ILT toegevoegd. In 2014 werden de Kernfysische dienst en het team adviesnetwerken (nucleair) overgeplaatst naar de ANVS. In 2015 werd het Zelfstandig Bestuursorgaan CFW toegevoegd aan de ILT.</p>
+              <p className="read-more"><a href="#">Read more</a></p>
+            </div>
+            <div className="relevant-link">
+              <a href="#">http://www.ilent.nl/</a>
+            </div>
+            <div className="descriptive-image">
+              <img className="wiki-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Voertuig_Inspectie_Verkeer_en_Waterstaat_IVW.jpg/260px-Voertuig_Inspectie_Verkeer_en_Waterstaat_IVW.jpg"></img>
+            </div>
           </div>
         </div>
       </div>
