d101bf16c2414d8ae2bae5abad7563d331dbd6a7
Joep Meindertsma <joep@ontola.io>
2019-11-01 13:42:00 +0100

    Fix tags

--- front/src/Components/Tags.tsx
+++ front/src/Components/Tags.tsx
@@ -6,23 +6,27 @@ const Tags = (props: {tags: TopTags | undefined}) => {
   if (!props.tags) {
     return null
   }
-  return (
-    <div
-      style={{
-        marginBottom: '.4rem',
-      }}
-    >
-      <div style={{
-        color: colors.bluedark,
-        borderRadius: '5px',
-        background: colors.g1,
-        padding: '.1rem .2rem',
-        display: 'inline',
-      }}>
-        {props.tags["http://www.w3.org/1999/02/22-rdf-syntax-ns#_0"]["https://argu.co/ns/meeting/tag"]}
+  try {
+    return (
+      <div
+        style={{
+          marginBottom: '.4rem',
+        }}
+      >
+        <div style={{
+          color: colors.bluedark,
+          borderRadius: '5px',
+          background: colors.g1,
+          padding: '.1rem .2rem',
+          display: 'inline',
+        }}>
+          {props.tags["http://www.w3.org/1999/02/22-rdf-syntax-ns#_0"]["https://argu.co/ns/meeting/tag"]}
+        </div>
       </div>
-    </div>
-  );
+    );}
+  catch {
+    return null
+  }
 };
 
 export default Tags;
