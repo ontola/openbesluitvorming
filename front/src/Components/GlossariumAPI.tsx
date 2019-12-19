d00bb357328338cf97f0292644e185ffc67ee12a
Hendrik Grondijs <hendrik@stamkracht.com>
2019-12-19 14:32:19 +0100

    ambiguous wikipedia results are hidden

--- front/src/Components/GlossariumAPI.tsx
+++ front/src/Components/GlossariumAPI.tsx
@@ -47,9 +47,13 @@ class GlossariumAPI {
     try {
       const response = await fetch(apiQuery);
       const data = await response.json();
+      console.log(data);
       const firstPageKey = Object.keys(data.query.pages)[0];
       const page = data.query.pages[firstPageKey];
       if (firstPageKey == "-1") {
+        return false;
+      }
+      if ('disambiguation' in page.pageprops) {
         return false;
       }
       const extract: string = page.extract;
