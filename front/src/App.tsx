34401b665cc85797a926a3fd6e7198e0a722f004
Joep Meindertsma <joep@argu.co>
2019-12-31 10:48:24 +0100

    Add social previews with Helmet #94

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,20 +2,30 @@ import LinkDevTools from "@ontola/link-devtools";
 import { RenderStoreProvider } from "link-redux";
 import React, { Component } from "react";
 import { Router } from "react-router";
+import { Helmet } from "react-helmet";
 
 import SearchRoute from "./Routes/SearchRoute";
 import history from "./helpers/history";
 import "./App.scss";
 import LRS from "./LRS";
 import "./Components/Views";
+import { IS_ORI } from "./config";
 
 class App extends Component {
   render() {
     return (
       <RenderStoreProvider value={LRS}>
-        <Router history={history}>
-          <SearchRoute />
-        </Router>
+        <React.Fragment>
+          <Helmet>
+            <meta property="og:description" content="Doorzoek vergaderstukken van meer dan 120 gemeenten en provincies." />
+            <meta property="og:image" content="/screenshot.png" />
+            <meta property="og:title" content={IS_ORI ? 'Open Raadsinformatie' : 'OpenBesluitvorming.nl'} />
+            <meta property="og:url" content={IS_ORI ? 'https://zoek.openraadsinformatie.nl' : 'https://openbesluitvorming.nl'} />
+          </Helmet>
+          <Router history={history}>
+            <SearchRoute />
+          </Router>
+        </React.Fragment>
       </RenderStoreProvider>
     );
   }
