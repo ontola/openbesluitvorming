0e32a22b2f9de483e23a01ae17b1765d3b948e90
Joep Meindertsma <joep@ontola.io>
2024-11-20 14:11:54 +0100

    Move to deno

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
-export const NODE_ENV = process.env.NODE_ENV;
 export const SERVER_PORT = 8080;
-export const FRONTEND_URL = process.env.FRONTEND_URL ||
-  "http://openbesluitvorming.nl";
+export const FRONTEND_URL = "http://openbesluitvorming.nl";
 export const FRONTEND_ACCEPT = "application/n-quads";
 export const API = "https://api.openraadsinformatie.nl/v1/elastic";
 /**
